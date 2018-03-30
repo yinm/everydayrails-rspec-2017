a887fcfe67866e3615ac11925c09205b4b5cf69e
yinm <yinm.cminor9@gmail.com>
2018-03-30 23:25:28 +0900

    Add validation test

--- 02-setup/spec/models/user_spec.rb
+++ 02-setup/spec/models/user_spec.rb
@@ -24,7 +24,11 @@ RSpec.describe User, type: :model do
     expect(user.errors[:last_name]).to include("can't be blank")
   end
 
-  it "is invalid without an email address"
+  it "is invalid without an email address" do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
 
   it "is invalid with a duplicate email address" do
     User.create(
