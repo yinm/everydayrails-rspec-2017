e7f6b50fef168ff1af0374b35c3b9460639fa8ae
yinm <yinm.cminor9@gmail.com>
2018-03-31 13:58:11 +0900

    Add status code example

--- 04-factories/spec/controllers/home_controller_spec.rb
+++ 04-factories/spec/controllers/home_controller_spec.rb
@@ -4,7 +4,12 @@ RSpec.describe HomeController, type: :controller do
   describe "#index" do
     it "responds successfully" do
       get :index
-      expect(response).to_not be_success
+      expect(response).to be_success
+    end
+
+    it "returns a 200 response" do
+      get :index
+      expect(response).to have_http_status "200"
     end
   end
 end
