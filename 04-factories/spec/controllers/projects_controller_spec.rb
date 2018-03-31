73d9e691696379467821a92212a359a6e1cf1516
yinm <yinm.cminor9@gmail.com>
2018-03-31 14:20:21 +0900

    Add #show examples

--- 04-factories/spec/controllers/projects_controller_spec.rb
+++ 04-factories/spec/controllers/projects_controller_spec.rb
@@ -32,4 +32,33 @@ RSpec.describe ProjectsController, type: :controller do
       end
     end
   end
+
+  describe "#show" do
+    context "as an authorized user" do
+      before do
+        @user = FactoryGirl.create(:user)
+        @project = FactoryGirl.create(:project, owner: @user)
+      end
+
+      it "responds successfully" do
+        sign_in @user
+        get :show, params: { id: @project.id }
+        expect(response).to be_success
+      end
+    end
+
+    context "as an unauthorized user" do
+      before do
+        @user = FactoryGirl.create(:user)
+        other_user = FactoryGirl.create(:user)
+        @project = FactoryGirl.create(:project, owner: other_user)
+      end
+
+      it "redirects to the dashboard" do
+        sign_in @user
+        get :show, params: { id: @project.id }
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
 end
