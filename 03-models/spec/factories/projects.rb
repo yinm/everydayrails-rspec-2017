41d5bc93af1fc85a990bddc0438ac47688ec3a58
yinm <yinm.cminor9@gmail.com>
2018-03-31 11:59:43 +0900

    Check association

--- 03-models/spec/factories/projects.rb
+++ 03-models/spec/factories/projects.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :project do
     sequence(:name) { |n| "Project #{n}" }
     description "A test project."
-    due_on 1.week.from.now
+    due_on 1.week.from_now
     association :owner
   end
 end
