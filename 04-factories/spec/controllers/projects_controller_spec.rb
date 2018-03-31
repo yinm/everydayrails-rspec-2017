8a947e490aaf1451cf94b60bde1e1166732899da
yinm <yinm.cminor9@gmail.com>
2018-03-31 20:04:16 +0900

    Add examples of delete method

--- 04-factories/spec/controllers/projects_controller_spec.rb
+++ 04-factories/spec/controllers/projects_controller_spec.rb
@@ -150,4 +150,63 @@ RSpec.describe ProjectsController, type: :controller do
       end
     end
   end
+
+  describe "#destroy" do
+    context "as an authorized user" do
+      before do
+        @user = FactoryGirl.create(:user)
+        @project = FactoryGirl.create(:project, owner: @user)
+      end
+
+      it "deletes a project" do
+        sign_in @user
+        expect {
+          delete :destroy, params: { id: @project.id }
+        }.to change(@user.projects, :count).by(-1)
+      end
+    end
+
+    context "as an unauthorized user" do
+      before do
+        @user = FactoryGirl.create(:user)
+        other_user = FactoryGirl.create(:user)
+        @project = FactoryGirl.create(:project, owner: other_user)
+      end
+
+      it "does not delete the project" do
+        sign_in @user
+        expect{
+          delete :destroy, params: { id: @project.id }
+        }.to_not change(Project, :count)
+      end
+
+      it "redirects to the dashboard" do
+        sign_in @user
+        delete :destroy, params: { id: @project.id }
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context "as a guest" do
+      before do
+        @project = FactoryGirl.create(:project)
+      end
+
+      it "returns a 302 response" do
+        delete :destroy, params: { id: @project.id }
+        expect(response).to have_http_status "302"
+      end
+
+      it "redirects to the sign-in page" do
+        delete :destroy, params: { id: @project.id }
+        expect(response).to redirect_to "/users/sign_in"
+      end
+
+      it "does not delete the project" do
+        expect {
+          delete :destroy, params: { id: @project.id }
+        }.to_not change(Project, :count)
+      end
+    end
+  end
 end
