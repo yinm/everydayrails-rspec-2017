b0de8bac3240bcc80b73430af30babc7eb879103
yinm <yinm.cminor9@gmail.com>
2018-03-31 20:43:28 +0900

    Add #edit examples

--- 04-factories/spec/controllers/projects_controller_spec.rb
+++ 04-factories/spec/controllers/projects_controller_spec.rb
@@ -130,6 +130,51 @@ RSpec.describe ProjectsController, type: :controller do
     end
   end
 
+  describe "#edit" do
+    context "as an authorized user" do
+      before do
+        @user = FactoryGirl.create(:user)
+        @project = FactoryGirl.create(:project, owner: @user)
+      end
+
+      it "responds successfully" do
+        sign_in @user
+        get :edit, params: { id: @project.id }
+        expect(response).to be_success
+      end
+    end
+
+    context "as an unauthorized user" do
+      before do
+        @user = FactoryGirl.create(:user)
+        other_user = FactoryGirl.create(:user)
+        @project = FactoryGirl.create(:project, owner: other_user)
+      end
+
+      it "redirects to the dashboard" do
+        sign_in @user
+        get :edit, params: { id: @project.id }
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context "as a guest" do
+      before do
+        @project = FactoryGirl.create(:project)
+      end
+
+      it "returns a 302 response" do
+        get :edit, params: { id: @project.id }
+        expect(response).to have_http_status "302"
+      end
+
+      it "redirects to the sign-in page" do
+        get :edit, params: { id: @project.id }
+        expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+  end
+
   describe "#update" do
     context "as an authorized user" do
       before do
