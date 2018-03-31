a12cb8b8f70056fb1029bcfdc8abc97c29728699
yinm <yinm.cminor9@gmail.com>
2018-03-31 14:12:43 +0900

    Add examples of as a guest

--- 04-factories/spec/controllers/projects_controller_spec.rb
+++ 04-factories/spec/controllers/projects_controller_spec.rb
@@ -2,20 +2,34 @@ require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
   describe "#index" do
-    before do
-      @user = FactoryGirl.create(:user)
+    context "as an authenticated user" do
+      before do
+        @user = FactoryGirl.create(:user)
+      end
+
+      it "responds successfully" do
+        sign_in @user
+        get :index
+        expect(response).to be_success
+      end
+
+      it "returns a 200 response" do
+        sign_in @user
+        get :index
+        expect(response).to have_http_status "200"
+      end
     end
 
-    it "responds successfully" do
-      sign_in @user
-      get :index
-      expect(response).to be_success
-    end
+    context "as a guest" do
+      it "returns a 302 response" do
+        get :index
+        expect(response).to have_http_status "302"
+      end
 
-    it "returns a 200 response" do
-      sign_in @user
-      get :index
-      expect(response).to have_http_status "200"
+      it "redirects to the sign-in page" do
+        get :index
+        expect(response).to redirect_to "/users/sign_in"
+      end
     end
   end
 end
