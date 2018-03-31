5179dfd9ac6e42bf92434d7873c59c6e9e59804b
yinm <yinm.cminor9@gmail.com>
2018-03-31 20:35:48 +0900

    Add #new examples

--- 04-factories/spec/controllers/projects_controller_spec.rb
+++ 04-factories/spec/controllers/projects_controller_spec.rb
@@ -62,6 +62,32 @@ RSpec.describe ProjectsController, type: :controller do
     end
   end
 
+  describe "#new" do
+    context "as an authenticated user" do
+      before do
+        @user = FactoryGirl.create(:user)
+      end
+
+      it "responds successfully" do
+        sign_in @user
+        get :new
+        expect(response).to be_success
+      end
+    end
+
+    context "as a guest" do
+      it "returns a 302 response" do
+        get :new
+        expect(response).to have_http_status "302"
+      end
+
+      it "redirects to the sign-in page" do
+        get :new
+        expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+  end
+
   describe "#create" do
     context "as an authenticated user" do
       before do
