18558f16f1c7badb5abf1bda4c7cc9a4ed9b092b
yinm <yinm.cminor9@gmail.com>
2018-03-31 14:32:44 +0900

    Add exapmles of post method

--- 04-factories/spec/controllers/projects_controller_spec.rb
+++ 04-factories/spec/controllers/projects_controller_spec.rb
@@ -61,4 +61,36 @@ RSpec.describe ProjectsController, type: :controller do
       end
     end
   end
+
+  describe "#create" do
+    context "as an authenticated user" do
+      before do
+        @user = FactoryGirl.create(:user)
+      end
+
+      it "adds a project" do
+        project_params = FactoryGirl.attributes_for(:project)
+        p project_params
+
+        sign_in @user
+        expect {
+          post :create, params: { project: project_params }
+        }.to change(@user.projects, :count).by(1)
+      end
+    end
+
+    context "as a guest" do
+      it "returns a 302 response" do
+        project_params = FactoryGirl.attributes_for(:project)
+        post :create, params: { project: project_params }
+        expect(response).to have_http_status "302"
+      end
+
+      it "redirects to the sign-in page" do
+        project_params = FactoryGirl.attributes_for(:project)
+        post :create, params: { project: project_params }
+        expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+  end
 end
