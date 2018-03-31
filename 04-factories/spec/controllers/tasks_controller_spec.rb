ccb880238c84fda849fbf1d11386e59472f9f533
yinm <yinm.cminor9@gmail.com>
2018-03-31 20:24:20 +0900

    Add create tests

--- 04-factories/spec/controllers/tasks_controller_spec.rb
+++ 04-factories/spec/controllers/tasks_controller_spec.rb
@@ -15,4 +15,33 @@ RSpec.describe TasksController, type: :controller do
       expect(response.content_type).to eq "application/json"
     end
   end
+
+  describe "#create" do
+    it "responds with JSON formatted output" do
+      new_task = { name: "New test task" }
+      sign_in @user
+      post :create, format: :json,
+        params: { project_id: @project.id, task: new_task }
+      expect(response.content_type).to eq "application/json"
+    end
+
+    it "adds a new task to the project" do
+      new_task = { name: "New test task" }
+      sign_in @user
+      expect {
+        post :create, format: :json,
+          params: { project_id: @project.id, task: new_task }
+      }.to change(@project.tasks, :count).by(1)
+    end
+  end
+
+  it "requires authentication" do
+    new_task = { name: "New test task" }
+    # do not login on purpose
+    expect {
+      post :create, format: :json,
+        params: { project_id: @project.id, task: new_task }
+    }.to_not change(@project.tasks, :count)
+    expect(response).to_not be_success
+  end
 end
