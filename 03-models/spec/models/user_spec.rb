d1b67f44291f910576ae6bd29244de43b106108e
yinm <yinm.cminor9@gmail.com>
2018-03-31 11:26:39 +0900

    Update examples with factory

--- 03-models/spec/models/user_spec.rb
+++ 03-models/spec/models/user_spec.rb
@@ -16,19 +16,19 @@ RSpec.describe User, type: :model do
   end
 
   it "is invalid without a first name" do
-    user = User.new(first_name: nil)
+    user = FactoryGirl.build(:user, first_name: nil)
     user.valid?
     expect(user.errors[:first_name]).to include("can't be blank")
   end
 
   it "is invalid without a last name" do
-    user = User.new(last_name: nil)
+    user = FactoryGirl.build(:user, last_name: nil)
     user.valid?
     expect(user.errors[:last_name]).to include("can't be blank")
   end
 
   it "is invalid without an email address" do
-    user = User.new(email: nil)
+    user = FactoryGirl.build(:user, email: nil)
     user.valid?
     expect(user.errors[:email]).to include("can't be blank")
   end
