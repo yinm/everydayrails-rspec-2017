449b70c6c144e18cd724a5171f736fcb3483ee36
yinm <yinm.cminor9@gmail.com>
2018-03-31 12:30:40 +0900

    Check inherit

--- 03-models/spec/factories/projects.rb
+++ 03-models/spec/factories/projects.rb
@@ -4,26 +4,17 @@ FactoryGirl.define do
     description "Sample project for testing purposes"
     due_on 1.week.from_now
     association :owner
-  end
 
-  factory :project_due_yesterday, class: Project do
-    sequence(:name) { |n| "Test Project #{n}" }
-    description "Sample project for testing purposes"
-    due_on 1.day.ago
-    association :owner
-  end
+    factory :project_due_yesterday do
+      due_on 1.day.ago
+    end
 
-  factory :project_due_today, class: Project do
-    sequence(:name) { |n| "Test Project #{n}" }
-    description "Sample project for testing purposes"
-    due_on Date.current.in_time_zone
-    association :owner
-  end
+    factory :project_due_today do
+      due_on Date.current.in_time_zone
+    end
 
-  factory :project_due_tomorrow, class: Project do
-    sequence(:name) { |n| "Test Project #{n}" }
-    description "Sample project for testing purposes"
-    due_on 1.day.from_now
-    association :owner
+    factory :project_due_tomorrow do
+      due_on 1.day.from_now
+    end
   end
 end
