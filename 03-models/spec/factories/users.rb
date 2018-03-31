8007217332c7da0cdca64d25978a8a85c86297ed
yinm <yinm.cminor9@gmail.com>
2018-03-31 11:41:33 +0900

    Check sequence

--- 03-models/spec/factories/users.rb
+++ 03-models/spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     first_name "Aaron"
     last_name "Sumner"
-    email "tester@example.com"
+    sequence(:email) { |n| "tester#{n}@example.cm" }
     password "dottle-nouveau-pavilion-tights-furze"
   end
 end
