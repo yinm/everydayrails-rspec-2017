6d6c2ecb42f8933d4b94a614fb7e6e2a752f62a2
yinm <yinm.cminor9@gmail.com>
2018-06-09 14:09:31 +0900

    Check debug for feature specs

--- 05-controllers/spec/features/projects_spec.rb
+++ 05-controllers/spec/features/projects_spec.rb
@@ -21,4 +21,10 @@ RSpec.feature "Projects", type: :feature do
       expect(page).to have_content "Owner: #{user.name}"
     }.to change(user.projects, :count).by(1)
   end
+
+  scenario "guest adds a project" do
+    visit projects_path
+    save_and_open_page
+    click_link "New Project"
+  end
 end
