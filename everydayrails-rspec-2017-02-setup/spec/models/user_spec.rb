dab4ceabf901f9f06e0d92e2e1ec135f6aba7b5a
yinm <yinm.cminor9@gmail.com>
2018-12-30 10:42:06 +0900

    Test validations

--- everydayrails-rspec-2017-02-setup/spec/models/user_spec.rb
+++ everydayrails-rspec-2017-02-setup/spec/models/user_spec.rb
@@ -11,13 +11,36 @@ RSpec.describe User, type: :model do
     expect(user).to be_valid
   end
 
-  it 'is invalid without a first name'
+  it 'is invalid without a first name' do
+    user = User.new(first_name: nil)
+    user.valid?
+    expect(user.errors[:first_name]).to include("can't be blank")
+  end
 
-  it 'is invalid without a last name'
+  it 'is invalid without a last name' do
+    user = User.new(last_name: nil)
+    user.valid?
+    expect(user.errors[:last_name]).to include("can't be blank")
+  end
 
   it 'is invalid without an email address'
 
-  it 'is invalid with a duplicate email address'
+  it 'is invalid with a duplicate email address' do
+    User.create(
+      first_name: 'Joe',
+      last_name: 'Tester',
+      email: 'tester@example.com',
+      password: 'dottle'
+    )
+    user = User.new(
+      first_name: 'Jane',
+      last_name: 'Tester',
+      email: 'tester@example.com',
+      password: 'dottle'
+    )
+    user.valid?
+    expect(user.errors[:email]).to include('has already been taken')
+  end
 
   it "returns a user's full name as a string"
 end
