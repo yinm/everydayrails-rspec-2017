65c51061227403392921d35a65435c2cb0f63253
yinm <yinm.cminor9@gmail.com>
2018-12-31 17:12:14 +0900

    Add factories

--- everydayrails-rspec-2017-03-models/spec/factories/projects.rb
+++ everydayrails-rspec-2017-03-models/spec/factories/projects.rb
@@ -1,8 +1,29 @@
 FactoryBot.define do
   factory :project do
-    sequence(:name) { |n| "Project #{n}" }
-    description 'A test project.'
+    sequence(:name) { |n| "Test Project #{n}" }
+    description "Sample project for testing purpose"
     due_on 1.week.from_now
+    association :owner
+  end
+
+  factory :project_due_yesterday, class: Project do
+    sequence(:name) { |n| "Test Project #{n}" }
+    description "Sample project for testing purpose"
+    due_on 1.day.ago
+    association :owner
+  end
+
+  factory :project_due_today, class: Project do
+    sequence(:name) { |n| "Test Project #{n}" }
+    description "Sample project for testing purpose"
+    due_on Date.current.in_time_zone
+    association :owner
+  end
+
+  factory :project_due_tomorrow, class: Project do
+    sequence(:name) { |n| "Test Project #{n}" }
+    description "Sample project for testing purpose"
+    due_on 1.day.from_now
     association :owner
   end
 end
