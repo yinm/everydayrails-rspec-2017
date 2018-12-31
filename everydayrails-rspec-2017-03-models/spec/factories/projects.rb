1350945a59b8ada7d14dfd94fe660bdcef6d6146
yinm <yinm.cminor9@gmail.com>
2018-12-31 17:16:22 +0900

    Check inherit

--- everydayrails-rspec-2017-03-models/spec/factories/projects.rb
+++ everydayrails-rspec-2017-03-models/spec/factories/projects.rb
@@ -4,26 +4,17 @@ FactoryBot.define do
     description "Sample project for testing purpose"
     due_on 1.week.from_now
     association :owner
-  end
 
-  factory :project_due_yesterday, class: Project do
-    sequence(:name) { |n| "Test Project #{n}" }
-    description "Sample project for testing purpose"
-    due_on 1.day.ago
-    association :owner
-  end
+    factory :project_due_yesterday do
+      due_on 1.day.ago
+    end
 
-  factory :project_due_today, class: Project do
-    sequence(:name) { |n| "Test Project #{n}" }
-    description "Sample project for testing purpose"
-    due_on Date.current.in_time_zone
-    association :owner
-  end
+    factory :project_due_today do
+      due_on Date.current.in_time_zone
+    end
 
-  factory :project_due_tomorrow, class: Project do
-    sequence(:name) { |n| "Test Project #{n}" }
-    description "Sample project for testing purpose"
-    due_on 1.day.from_now
-    association :owner
+    factory :project_due_tomorrow do
+      due_on 1.day.from_now
+    end
   end
 end
