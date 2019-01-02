7e67c7ae5a06e19c0d4fdcf03bd82372acb03aba
yinm <yinm.cminor9@gmail.com>
2019-01-02 19:11:56 +0900

    Check request spec of POST

--- everydayrails-rspec-2017-06-features/spec/requests/projects_api_spec.rb
+++ everydayrails-rspec-2017-06-features/spec/requests/projects_api_spec.rb
@@ -25,4 +25,20 @@ RSpec.describe "Project API", type: :request do
     json = JSON.parse(response.body)
     expect(json['name']).to eq 'Second Sample Project'
   end
+
+  it 'creates a project' do
+    user = FactoryBot.create(:user)
+
+    project_attributes = FactoryBot.attributes_for(:project)
+
+    expect do
+      post api_projects_path, params: {
+        user_email: user.email,
+        user_token: user.authentication_token,
+        project: project_attributes
+      }
+    end.to change(user.projects, :count).by(1)
+
+    expect(response).to have_http_status(:success)
+  end
 end
