98bfaaf148ba86121b25457a7d1a44d9e55bffea
yinm <yinm.cminor9@gmail.com>
2019-01-03 18:00:38 +0900

    Add aggregated failure examples

--- everydayrails-rspec-2017-07-requests/spec/features/projects_spec.rb
+++ everydayrails-rspec-2017-07-requests/spec/features/projects_spec.rb
@@ -12,9 +12,11 @@ RSpec.feature "Projects", type: :feature do
       fill_in "Description", with: "Trying out Capybara"
       click_button "Create Project"
 
-      expect(page).to have_content "Project was successfully created"
-      expect(page).to have_content "Test Project"
-      expect(page).to have_content "Owner: #{user.name}"
+      aggregate_failures do
+        expect(page).to have_content "Project was successfully created"
+        expect(page).to have_content "Test Project"
+        expect(page).to have_content "Owner: #{user.name}"
+      end
     }.to change(user.projects, :count).by(1)
   end
 end
