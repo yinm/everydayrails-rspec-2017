5331130185b70beadcf845b0bb9715dbaa4e97a1
yinm <yinm.cminor9@gmail.com>
2019-01-04 22:11:14 +0900

    Add basic test of external service integration

--- everydayrails-rspec-2017-09-test-faster/spec/models/user_spec.rb
+++ everydayrails-rspec-2017-09-test-faster/spec/models/user_spec.rb
@@ -31,4 +31,13 @@ RSpec.describe User, type: :model do
     user = FactoryBot.create(:user)
     expect(UserMailer).to have_received(:welcome_email).with(user)
   end
+
+  it 'performs geocoding' do
+    user = FactoryBot.create(:user, last_sign_in_ip: '161.185.207.20')
+    expect do
+      user.geocode
+    end.to change(user, :location).
+      from(nil).
+      to('Brooklyn, New York, US')
+  end
 end
